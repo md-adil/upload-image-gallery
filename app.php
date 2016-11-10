4bf353cb911de242745d3cfc1460409a93330e75
Md Adil <md-adil@live.com>
2016-11-10 14:53:36 +0530

    Adding upload system

--- app.php
+++ app.php
@@ -1,51 +1,14 @@
 <?php
 require('vendor/autoload.php');
+use App\Upload;
 
 $args = $argv;
 array_shift($args);
+
 (new Dotenv\Dotenv($_SERVER['USERPROFILE'], '.upload-gallery'))->load();
 
-$upload_dir = 'uploaded_files';
-$link_file = 'uploaded_links.txt';
+$client = new GuzzleHttp\Client(['verify'=>false]);
 
-
-$client = new GuzzleHttp\Client();
-
-// die(print_r(openssl_get_cert_locations(),1));
-
-if(!is_dir($upload_dir)) {
-	mkdir($upload_dir);
-}
-
-foreach(glob('*.*') as $file) {
-	if($file == $link_file) continue;
-	echo 'uploading: ' . $file . "\n";
-	// $file = realpath($file);
-	// die();
-	// $res = $client->api('image')->upload([
-	// 	'image' => file_get_contents($file),
- //    	'type'  => 'base64',
-	// ]);
-
-	uploadImg($file);
-	die();
-	echo 'Uploaded success' . "\n";
-	file_put_contents($link_file, $file . PHP_EOL, FILE_APPEND);
-	rename($file, $upload_dir . '/' . $file);
-}
-function uploadImg($img) {
-	$clientId = getenv('CLIENT_ID');
-	$client = $GLOBALS['client'];
-	$url = 'https://api.imgur.com/3/image.json';
-	$req = $client->request('POST', $url, [
-		'verify' => false,
-	    'headers' => [
-	        'Authorization' => 'Client-ID ' . $clientId
-	    ],
-	    'form_params' => [
-	    	'image' => file_get_contents($img)
-	    ]
-	]);
-
-	print_r($req->getBody()->read(1024));
-}
+$uploader = new Upload($client);
+$uploader->upload($args);
+// print_r($uploader->findFiles());
---
+++ app/Upload.php
@@ -0,0 +1,93 @@
+<?php
+namespace App;
+/**
+* 
+*/
+class Upload
+{
+	protected $client;
+	protected $upload_dir = 'uploaded_files';
+	protected $upload_links = 'uploaded_links.txt';
+	protected $upload_links_map = 'uploaded_links.csv';
+	protected $ignore_files = [
+		'uploaded_files', 'uploaded_links.txt', 'uploaded_links.csv'
+	];
+
+	protected $clientId;
+	protected $uploadUrl = 'https://api.imgur.com/3/image.json';
+
+	function __construct($client, $clientId = null, $clientSecret = null)
+	{
+		$this->client = $client;
+		$this->clientId = $clientId ?: getenv('CLIENT_ID');
+		$this->clientSecret = $clientSecret ?: getenv('CLIENT_ID');
+	}
+
+	public function println() {
+		echo implode(' ', func_get_args()) . PHP_EOL;
+	}
+
+	public function upload($files = []) {
+		$this->createUploadsDir();
+		if(!$files) {
+			$files = $this->findFiles();
+		}
+
+		$this->println("Total files to upload: ", count($files));
+
+		foreach ($files as $file) {
+			$this->println("Uploading: ", $file);
+			try {
+				$upload = $this->uploadToImgur($file);
+				if($upload && $upload->status == 200) {
+					$this->storeLink($upload->data->link, $file);
+					$this->println("Uploaded: ", $upload->data->link);
+					$this->moveFile($file);
+				} else {
+					$this->println("Something went wrong.");
+				}
+			} catch (\Exception $e) {
+				$this->println("Error: ", $e->getMessage());
+			}
+		}
+	}
+
+	public function uploadToImgur($file) {
+		$res = $this->client->request('POST', $this->uploadUrl, [
+			'verify' => false,
+		    'headers' => [
+		        'Authorization' => 'Client-ID ' . $this->clientId
+		    ],
+		    'form_params' => [
+		    	'image' => base64_encode(file_get_contents($file))
+		    ]
+		]);
+
+		if($res) {
+			return json_decode($res->getBody()->read(1024));
+		}
+	}
+
+
+	public function findFiles() {
+		$files = glob('*.*');
+		if(!$files) return [];
+		return array_diff($files, $this->ignore_files);
+	}
+
+	public function createUploadsDir() {
+		if(!is_dir($this->upload_dir)) {
+			mkdir($this->upload_dir);
+		}
+	}
+
+	public function moveFile($file) {
+		rename($file, $this->upload_dir . '/' . $file);
+	}
+
+	public function storeLink($link, $file) {
+		file_put_contents($this->upload_links, $link . PHP_EOL, FILE_APPEND);
+		file_put_contents($this->upload_links_map, $link . ',' . $file . PHP_EOL, FILE_APPEND);
+	}
+
+}
